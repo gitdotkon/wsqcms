b96ec420ef82e741ea1f295dbda93bb5cf1933ea
Liubing <liubing@flow.asia>
2016-11-22 09:53:29 +0800

    Change 360 image

--- wp-content/themes/wanda-studios/parts/home/first-screen.php
+++ wp-content/themes/wanda-studios/parts/home/first-screen.php
@@ -28,11 +28,7 @@ $mobile_bg = get_field('mobile_background');
                 </div>
                 <div class="all_view_wrapper">
                     <a href="<?php echo $map; ?>" data-id="<?php echo $map; ?>" target="_blank" class="go-to lightbox_btn">
-                        <?php if(ICL_LANGUAGE_CODE == 'en'): ?>
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/360_view.png" class="all_view" alt=""/>
-                        <?php else: ?>
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/360_view_ch.png" class="all_view" alt=""/>
-                        <?php endif; ?>
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/360_view_<?php echo ICL_LANGUAGE_CODE;?>.png" class="all_view" alt=""/>
                     </a>
                 </div>
             </div>
